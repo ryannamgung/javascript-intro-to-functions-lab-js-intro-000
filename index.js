26e9a9c997fc431e730b70fcfea2728c6d83bac0
Ryan Namgung <ryannamgung@gmail.com>
2020-11-08 22:28:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   if(string.toUpperCase() == string){
-    return "YES INDEED";
+    return "YES INDEED!";
   }else if(string.toLowerCase() == string)
   {
     return "I can't hear you!";
