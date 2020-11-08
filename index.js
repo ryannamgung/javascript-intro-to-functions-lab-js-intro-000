9b417ac3a9a3db0e811030d11dcc3b3d3547db09
Ryan Namgung <ryannamgung@gmail.com>
2020-11-08 22:26:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string)
+{
+  return string.toUpperCase();
+}
+
+function whisper(string)
+{
+  return string.toLowerCase();
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string)
+{
+  console.log(stirng.toLowerCase());
+}
+
+function sayHiToGrandma(string)
+{
+  switch(string)
+  {
+    case(string.toUpperCase() == string):
+      return "YES INDEED";
+      break;
+    case(string.toLowerCase() == string):
+      return "I can't hear you!";
+      break
+    case(string == "I love you, Grandma."):
+      return "I love you, too."
+  }
+}
